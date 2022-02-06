874061eb062c951269651872106194e61a3f7fb9
Sulav <60843792+sulavthapa197@users.noreply.github.com>
2022-02-06 22:05:11 +0545

    Edited the codes and comments.
    Added clearer definitions within the code and comments. Reviewed the proper functioning and also checked if the range of the input can be increased or not.

--- CircularArray.py
+++ CircularArray.py
@@ -3,43 +3,47 @@ import os
 
 while True: #while loop with try except statement to limit the input range else the code breaks
     try:
-        n=int(input("enter the number of rows you want to generate: ")) # n also repesents the range of numbers in first row
-        if n < 1 or n > 17: # n > 17 not possible because it breaks the structure of square arrray in commandline
+        num_row = int(input("Enter the number of rows you want to generate: "))
+        if num_row < 1 or num_row > 17:
             raise ValueError #sends back to input option
         break
     except ValueError:
         print("Invalid integer. The number must be in the range of 1-17 for correct execution.")#value error message
 
-def fun_spiral(n):#Funtion defination
-    arr=np.full((n,n),0) # generates nXn null array
-    a=1
-    low=0 #high-low representing the range of the numbers within array
-    high=n-1
-    count=int((n+1)/2)
-#combination of low,high seqeunce and os.system("cls") creates a circling effect in output
+def fun_spiral(num_row):# Function definition
+    arr=np.full((num_row,num_row),0) # generates nXn null array
+    a=1 # represents the number that is inserted into the list
+    low=0
+    high= num_row - 1
+    count=int((num_row+1)/2)
     for i in range(count): #nested for loops to print a 2D array
         for j in range(low,high+1):#Four respective for loops for each side of the square array
             arr[low,j]=a
             a+=1
-            os.system('cls') #os update clears the screen such that it gives an effect of spiralling number fillup
+            os.system('cls') #os update clears the screen such that it gives an effect of spiralling number fill up
             print(arr)
+        
         for j in range(low+1,high+1):
             arr[j,high]=a
             a+=1
             os.system('cls')
             print(arr)
+
         for j in range(high-1,low-1,-1):
             arr[high,j]=a
             a+=1
             os.system('cls')
             print(arr)
+
         for j in range(high-1,low,-1):
             arr[j,low]=a
             a+=1
             os.system('cls')
             print(arr)
-        low+=1
-        high-=1
+        
+        low+=1 # low/high adjusted such that the loops runs within a smaller square every time the main loop repeats 
+        high-=1 
+
     #print(arr)  
 
-fun_spiral(n)# Call for the function
+fun_spiral(num_row)# Call for the function
